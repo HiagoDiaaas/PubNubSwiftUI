a11c80f0bddd7e9f2f779ca0246a074c62b6a476
GregR <greggrichardson@live.com>
2022-09-22 20:39:16 -0400

    Initial Commit

---
+++ PLMControl/DevicesView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PLMControl
+//
+//  Created by Gregory Richardson on 9/22/22.
+//
+
+import SwiftUI
+
+struct DevicesView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        DevicesView()
+    }
+}
---
+++ PLMControl/PLMControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  PLMControlApp.swift
+//  PLMControl
+//
+//  Created by Gregory Richardson on 9/22/22.
+//
+
+import SwiftUI
+
+@main
+struct PLMControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            DevicesView()
+        }
+    }
+}
